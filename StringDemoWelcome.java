037da399059f418bf03f9cc88a5858d55c874e90
Surendhar M S <139234490+Surendhar2252@users.noreply.github.com>
2025-07-18 10:25:34 +0530

    Delete StringDemoWelcome.java

--- StringDemoWelcome.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class StringDemoWelcome {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        String s1=sc.nextLine();
-        System.out.println("Welcome "+s1);
-        sc.close();
-    }
-}
