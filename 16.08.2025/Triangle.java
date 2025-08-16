76d54767a57bfed8950da46d8ca26865765796e3
Surendhar M S <139234490+Surendhar2252@users.noreply.github.com>
2025-08-16 11:16:24 +0530

    Create Triangle.java

---
+++ 16.08.2025/Triangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Triangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+
+//input: 5
